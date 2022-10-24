9de3a1cae98bf8c191eec72bd85aeaaeb8487d1c
Lu Yu <nluyu@amazon.com>
2022-10-24 16:02:56 -0700

    fix file name

--- config/playground/docker/dev/scripts/update-config.sh
+++ config/playground/docker/dev/scripts/update-config.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "append config to yaml"
-source_file="/opt/opensearch-dashboards/extra_config/osd_config.yaml"
+source_file="/opt/opensearch-dashboards/extra_config/osd.yml"
 target_file="/usr/share/opensearch-dashboards/config/opensearch_dashboards.yml"
 
 cat $source_file >> $target_file
