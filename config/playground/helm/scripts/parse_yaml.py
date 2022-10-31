0482aa189c665377a31e97d996d4fc28e6273541
Lu Yu <nluyu@amazon.com>
2022-10-30 21:19:01 -0700

    add secret back
    Signed-off-by: Lu Yu <nluyu@amazon.com>

--- config/playground/helm/scripts/parse_yaml.py
+++ config/playground/helm/scripts/parse_yaml.py
@@ -6,5 +6,7 @@ if __name__ == "__main__":
     print("hey")
     with open("config/playground/helm/dev/helm-opensearch-dashboards.yaml", "r") as f:
         for r in f:
+            if "G-0SHJ0Q9ZBY" in r:
+                print("yes have it")
             print(r)
     print(os.environ['SOME_OTHER'])
