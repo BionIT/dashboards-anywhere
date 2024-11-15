5775afce48332866b42d4c05e3dbcc32227e34b2
Lu <yulu.nju@gmail.com>
2024-11-15 10:57:18 -0800

    test

--- src/playground/sample_data_tooling/sample_data_authentication/sample_data_authentication.py
+++ src/playground/sample_data_tooling/sample_data_authentication/sample_data_authentication.py
@@ -4,6 +4,7 @@ SPDX-License-Identifier: Apache-2.0
 """
 
 
+
 class Authentication:
     """
     Abstract class for returning headers. Nothing is defined here but there should be only one method: get_auth()
